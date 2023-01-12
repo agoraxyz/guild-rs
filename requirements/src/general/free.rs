e744f52e449e81fd5b2c24262f6889c893c2f0db
richardanyalai <gtaricsi@gmail.com>
2023-01-12 11:46:56 +0100

    Added Free requirement type

---
+++ requirements/src/general/free.rs
@@ -0,0 +1,29 @@
+use crate::{Error, Requirement, User};
+use async_trait::async_trait;
+
+pub struct FreeRequirement;
+
+#[async_trait]
+impl Requirement for FreeRequirement {
+    async fn check_for_many(&self, users: &[User]) -> Result<Vec<bool>, Error> {
+        Ok(users.iter().map(|_| true).collect())
+    }
+
+    async fn check(&self, user: User) -> Result<bool, Error> {
+        self.check_for_many(&[user]).await.map(|res| res[0])
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{FreeRequirement, Requirement, User};
+
+    #[tokio::test]
+    async fn free_requirement_check() {
+        async fn free_requirement_check() {
+            let req = FreeRequirement;
+
+            assert!(req.check(User { addresses: vec![] }).await.unwrap());
+        }
+    }
+}
