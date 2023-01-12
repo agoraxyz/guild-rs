6598ff9e0840a0111be046297a67b088a0b98af4
richardanyalai <gtaricsi@gmail.com>
2023-01-12 12:04:39 +0100

    Added macro to create addresses from Strings

--- requirements/src/lib.rs
+++ requirements/src/lib.rs
@@ -22,3 +22,11 @@ pub trait Requirement {
     async fn check_for_many(&self, users: &[User]) -> Result<Vec<bool>, Error>;
     async fn check(&self, user: User) -> Result<bool, Error>;
 }
+
+#[macro_export]
+macro_rules! address {
+    ($addr:expr) => {{
+        use std::str::FromStr;
+        web3::types::Address::from_str($addr).expect(&format!("Invalid address {}", $addr))
+    }};
+}
