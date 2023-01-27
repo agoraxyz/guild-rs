3bb951e8d7e3216772c3320d257a52a127183abf
richardanyalai <gtaricsi@gmail.com>
2023-01-27 01:01:34 +0100

    Added Balancy tests

--- providers/src/evm/balancy/mod.rs
+++ providers/src/evm/balancy/mod.rs
@@ -38,27 +38,26 @@ pub async fn get_address_tokens(
     chain: EvmChain,
     address: Address,
 ) -> Result<AddressTokenResponse, BalancyError> {
-    match CHAIN_IDS.get(&(chain as u32)) {
-        None => Err(BalancyError::ChainNotSupported(format!("{chain:?}"))),
-        Some(id) => {
-            let res = CLIENT
-                .read()
-                .await
-                .get(format!(
-                    "{BASE_URL}/{ADDRESS_TOKENS}{address:#x}{BALANCY_CHAIN}{id}"
-                ))
-                .send()
-                .await?;
+    let Some(id) = CHAIN_IDS.get(&(chain as u32)) else {
+        return Err(BalancyError::ChainNotSupported(format!("{chain:?}")));
+    };
 
-            let status = res.status();
+    let res = CLIENT
+        .read()
+        .await
+        .get(format!(
+            "{BASE_URL}/{ADDRESS_TOKENS}{address:#x}{BALANCY_CHAIN}{id}"
+        ))
+        .send()
+        .await?;
 
-            match status {
-                StatusCode::OK => Ok(res.json::<AddressTokenResponse>().await?),
-                StatusCode::BAD_REQUEST => Err(BalancyError::InvalidBalancyRequest),
-                StatusCode::TOO_MANY_REQUESTS => Err(BalancyError::TooManyRequests),
-                _ => Err(BalancyError::Unknown(status.as_u16())),
-            }
-        }
+    let status = res.status();
+
+    match status {
+        StatusCode::OK => Ok(res.json::<AddressTokenResponse>().await?),
+        StatusCode::BAD_REQUEST => Err(BalancyError::InvalidBalancyRequest),
+        StatusCode::TOO_MANY_REQUESTS => Err(BalancyError::TooManyRequests),
+        _ => Err(BalancyError::Unknown(status.as_u16())),
     }
 }
 
@@ -121,7 +120,7 @@ impl BalanceQuerier for BalancyProvider {
                     .erc1155
                     .iter()
                     .filter(|token| {
-                        token.addr == address && {
+                        token.address == address && {
                             match id {
                                 Some(id) => token.token_id == id,
                                 None => true,
@@ -130,29 +129,116 @@ impl BalanceQuerier for BalancyProvider {
                     })
                     .map(|token| token.amount)
                     .reduce(|a, b| a + b)
-                    .unwrap_or(U256::from(0));
+                    .unwrap_or_default();
 
                 Ok(res)
             }
-            TokenType::Coin => Err(BalancyError::TokenTypeNotSupported("COIN".to_string())),
+            TokenType::Coin => Err(BalancyError::TokenTypeNotSupported(format!(
+                "{token_type:?}"
+            ))),
         }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use std::str::FromStr;
+    use crate::{
+        evm::{
+            balancy::{get_address_tokens, BALANCY_PROVIDER},
+            EvmChain,
+        },
+        BalanceQuerier,
+    };
+    use ethereum_types::U256;
+    use rusty_gate_common::{address, TokenType::*};
 
-    use crate::{evm::balancy::get_address_tokens, evm::EvmChain};
-    use ethereum_types::Address;
+    const USER_1_ADDR: &str = "0xE43878Ce78934fe8007748FF481f03B8Ee3b97DE";
+    const USER_2_ADDR: &str = "0x14DDFE8EA7FFc338015627D160ccAf99e8F16Dd3";
+    const USER_3_ADDR: &str = "0x283d678711daa088640c86a1ad3f12c00ec1252e";
+    const ERC20_ADDR: &str = "0x458691c1692cd82facfb2c5127e36d63213448a8";
+    const ERC721_ADDR: &str = "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85";
+    const ERC721_ID: &str =
+        "61313325075603536901663283754390960556726744542208800735045237225934362163454";
+    const ERC1155_ADDR: &str = "0x76be3b62873462d2142405439777e971754e8e77";
+    const ERC1155_ID: &str = "10868";
 
     #[tokio::test]
     async fn balancy_address_tokens() {
-        assert!(get_address_tokens(
-            EvmChain::Ethereum,
-            Address::from_str("0xE43878Ce78934fe8007748FF481f03B8Ee3b97DE").unwrap()
-        )
-        .await
-        .is_ok());
+        assert!(
+            get_address_tokens(EvmChain::Ethereum, address!(USER_1_ADDR))
+                .await
+                .is_ok()
+        );
+    }
+
+    #[tokio::test]
+    async fn balancy_get_erc20_balance() {
+        let token_type = Fungible {
+            address: address!(ERC20_ADDR),
+        };
+
+        assert_eq!(
+            BALANCY_PROVIDER
+                .get_balance_for_one(token_type, address!(USER_2_ADDR))
+                .await
+                .unwrap(),
+            U256::from(100000000000000000000_u128)
+        );
+    }
+
+    #[tokio::test]
+    async fn balancy_get_erc721_balance() {
+        let token_type_without_id = NonFungible {
+            address: address!(ERC721_ADDR),
+            id: None,
+        };
+        let token_type_with_id = NonFungible {
+            address: address!(ERC721_ADDR),
+            id: Some(U256::from_dec_str(ERC721_ID).unwrap()),
+        };
+        let user_address = address!(USER_1_ADDR);
+
+        assert_eq!(
+            BALANCY_PROVIDER
+                .get_balance_for_one(token_type_without_id, user_address)
+                .await
+                .unwrap(),
+            U256::from(1)
+        );
+        assert_eq!(
+            BALANCY_PROVIDER
+                .get_balance_for_one(token_type_with_id, user_address)
+                .await
+                .unwrap(),
+            U256::from(1)
+        );
+    }
+
+    #[tokio::test]
+    async fn balancy_get_erc1155_balance() {
+        let token_type_without_id = Special {
+            address: address!(ERC1155_ADDR),
+            id: None,
+        };
+        let token_type_with_id = Special {
+            address: address!(ERC1155_ADDR),
+            id: Some(U256::from_dec_str(ERC1155_ID).unwrap()),
+        };
+        let user_address = address!(USER_3_ADDR);
+
+        assert_eq!(
+            BALANCY_PROVIDER
+                .get_balance_for_one(token_type_without_id, user_address)
+                .await
+                .unwrap(),
+            U256::from(6840)
+        );
+        assert_eq!(
+            BALANCY_PROVIDER
+                .get_balance_for_one(token_type_with_id, user_address)
+                .await
+                .unwrap(),
+            U256::from(16)
+        );
     }
 }
