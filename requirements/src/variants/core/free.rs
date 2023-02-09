d71013f5e618074ad4a92c11d0c2903ff80a382e
richardanyalai <gtaricsi@gmail.com>
2023-02-09 22:12:23 +0100

    Refactored Free Requirement

--- requirements/src/variants/core/free.rs
+++ requirements/src/variants/core/free.rs
@@ -1,31 +1,19 @@
 use crate::Requirement;
-use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
-use std::marker::{Send, Sync};
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Free;
 
-#[async_trait]
 impl Requirement for Free {
     type Error = ();
-    type Identity = Box<dyn Send + Sync>;
-    type Client = ();
+    type VerificationData = ();
 
-    async fn check_for_many(
-        &self,
-        _client: &Self::Client,
-        identities: &[Self::Identity],
-    ) -> Result<Vec<bool>, Self::Error> {
-        Ok(identities.iter().map(|_| true).collect())
+    fn verify(&self, _v: &Self::VerificationData) -> bool {
+        true
     }
 
-    async fn check(
-        &self,
-        _client: &Self::Client,
-        _identity: Self::Identity,
-    ) -> Result<bool, Self::Error> {
-        Ok(true)
+    fn verify_batch(&self, v: &[Self::VerificationData]) -> Vec<bool> {
+        vec![true; v.len()]
     }
 }
 
@@ -33,10 +21,10 @@ impl Requirement for Free {
 mod test {
     use super::{Free, Requirement};
 
-    #[tokio::test]
-    async fn free_requirement_check() {
+    #[test]
+    fn free_requirement_check() {
         let req = Free;
 
-        assert!(req.check(&(), Box::new(69)).await.unwrap());
+        assert!(req.verify(&()));
     }
 }
