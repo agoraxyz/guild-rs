5d57e9a270d4750417be443455a00f4ed33129ff
richardanyalai <gtaricsi@gmail.com>
2023-02-21 23:24:32 +0100

    Moved Relation to common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,6 +4,7 @@
 #![deny(unused_crate_dependencies)]
 
 use async_trait::async_trait;
+use core::ops::{Range, RangeInclusive};
 use primitive_types::H160 as Address;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
@@ -67,6 +68,31 @@ pub trait Retrievable {
     ) -> Result<Vec<Self::Res>, Self::Error>;
 }
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub enum Relation<T> {
+    EqualTo(T),
+    GreaterThan(T),
+    GreaterOrEqualTo(T),
+    LessThan(T),
+    LessOrEqualTo(T),
+    Between(Range<T>),
+    BetweenInclusive(RangeInclusive<T>),
+}
+
+impl<T: PartialEq + PartialOrd> Relation<T> {
+    pub fn assert(&self, x: &T) -> bool {
+        match self {
+            Relation::EqualTo(a) => x == a,
+            Relation::GreaterThan(a) => x > a,
+            Relation::GreaterOrEqualTo(a) => x >= a,
+            Relation::LessThan(a) => x < a,
+            Relation::LessOrEqualTo(a) => x <= a,
+            Relation::Between(range) => range.contains(x),
+            Relation::BetweenInclusive(range) => range.contains(x),
+        }
+    }
+}
+
 #[macro_export]
 macro_rules! address {
     ($addr:expr) => {{
@@ -77,5 +103,41 @@ macro_rules! address {
 
 #[cfg(test)]
 mod test {
+    use super::Relation;
     use shiba as _;
+
+    #[test]
+    fn relations() {
+        assert!(Relation::<u32>::EqualTo(0).assert(&0));
+        assert!(!Relation::<u32>::EqualTo(10).assert(&2));
+        assert!(!Relation::<u32>::EqualTo(420).assert(&421));
+
+        assert!(!Relation::<u32>::GreaterThan(10).assert(&3));
+        assert!(!Relation::<u32>::GreaterThan(10).assert(&10));
+        assert!(Relation::<u32>::GreaterThan(10).assert(&20));
+
+        assert!(Relation::<u32>::GreaterOrEqualTo(23).assert(&42));
+        assert!(Relation::<u32>::GreaterOrEqualTo(23).assert(&23));
+        assert!(!Relation::<u32>::GreaterOrEqualTo(23).assert(&14));
+
+        assert!(Relation::<u32>::LessThan(23).assert(&1));
+        assert!(!Relation::<u32>::LessThan(23).assert(&23));
+        assert!(!Relation::<u32>::LessThan(23).assert(&42));
+
+        assert!(Relation::<u32>::LessOrEqualTo(23).assert(&1));
+        assert!(Relation::<u32>::LessOrEqualTo(23).assert(&23));
+        assert!(!Relation::<u32>::LessOrEqualTo(23).assert(&42));
+
+        assert!(!Relation::<u32>::Between(0..100).assert(&230));
+        assert!(!Relation::<u32>::Between(50..100).assert(&15));
+        assert!(!Relation::<u32>::Between(50..100).assert(&100));
+        assert!(Relation::<u32>::Between(50..100).assert(&77));
+        assert!(Relation::<u32>::Between(50..100).assert(&50));
+
+        assert!(!Relation::<u32>::BetweenInclusive(0..=100).assert(&230));
+        assert!(!Relation::<u32>::BetweenInclusive(50..=100).assert(&15));
+        assert!(Relation::<u32>::BetweenInclusive(50..=100).assert(&100));
+        assert!(Relation::<u32>::BetweenInclusive(50..=100).assert(&77));
+        assert!(Relation::<u32>::BetweenInclusive(50..=100).assert(&50));
+    }
 }
--- requirements/src/variants/core/balance.rs
+++ requirements/src/variants/core/balance.rs
@@ -1,36 +1,12 @@
 use crate::{Requirement, RequirementError};
 use async_trait::async_trait;
-use core::ops::{Range, RangeInclusive};
-use guild_common::{Retrievable, TokenType};
-use guild_providers::evm::Provider;
-use guild_providers::{evm::EvmChain, BalanceQuerier};
+use guild_common::{Relation, Retrievable, TokenType};
+use guild_providers::{
+    evm::{EvmChain, Provider},
+    BalanceQuerier,
+};
 use primitive_types::{H160 as Address, U256};
 use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub enum Relation<T> {
-    EqualTo(T),
-    GreaterThan(T),
-    GreaterOrEqualTo(T),
-    LessThan(T),
-    LessOrEqualTo(T),
-    Between(Range<T>),
-    BetweenInclusive(RangeInclusive<T>),
-}
-
-impl<T: PartialEq + PartialOrd> Relation<T> {
-    pub fn assert(&self, x: &T) -> bool {
-        match self {
-            Relation::EqualTo(a) => x == a,
-            Relation::GreaterThan(a) => x > a,
-            Relation::GreaterOrEqualTo(a) => x >= a,
-            Relation::LessThan(a) => x < a,
-            Relation::LessOrEqualTo(a) => x <= a,
-            Relation::Between(range) => range.contains(x),
-            Relation::BetweenInclusive(range) => range.contains(x),
-        }
-    }
-}
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Balance<T, U, V> {
@@ -91,41 +67,6 @@ mod test {
     use super::{Balance, Relation, Requirement, TokenType, U256};
     use guild_common::address;
     use guild_providers::evm::EvmChain;
-
-    #[test]
-    fn relations() {
-        assert!(Relation::<u32>::EqualTo(0).assert(&0));
-        assert!(!Relation::<u32>::EqualTo(10).assert(&2));
-        assert!(!Relation::<u32>::EqualTo(420).assert(&421));
-
-        assert!(!Relation::<u32>::GreaterThan(10).assert(&3));
-        assert!(!Relation::<u32>::GreaterThan(10).assert(&10));
-        assert!(Relation::<u32>::GreaterThan(10).assert(&20));
-
-        assert!(Relation::<u32>::GreaterOrEqualTo(23).assert(&42));
-        assert!(Relation::<u32>::GreaterOrEqualTo(23).assert(&23));
-        assert!(!Relation::<u32>::GreaterOrEqualTo(23).assert(&14));
-
-        assert!(Relation::<u32>::LessThan(23).assert(&1));
-        assert!(!Relation::<u32>::LessThan(23).assert(&23));
-        assert!(!Relation::<u32>::LessThan(23).assert(&42));
-
-        assert!(Relation::<u32>::LessOrEqualTo(23).assert(&1));
-        assert!(Relation::<u32>::LessOrEqualTo(23).assert(&23));
-        assert!(!Relation::<u32>::LessOrEqualTo(23).assert(&42));
-
-        assert!(!Relation::<u32>::Between(0..100).assert(&230));
-        assert!(!Relation::<u32>::Between(50..100).assert(&15));
-        assert!(!Relation::<u32>::Between(50..100).assert(&100));
-        assert!(Relation::<u32>::Between(50..100).assert(&77));
-        assert!(Relation::<u32>::Between(50..100).assert(&50));
-
-        assert!(!Relation::<u32>::BetweenInclusive(0..=100).assert(&230));
-        assert!(!Relation::<u32>::BetweenInclusive(50..=100).assert(&15));
-        assert!(Relation::<u32>::BetweenInclusive(50..=100).assert(&100));
-        assert!(Relation::<u32>::BetweenInclusive(50..=100).assert(&77));
-        assert!(Relation::<u32>::BetweenInclusive(50..=100).assert(&50));
-    }
 
     #[tokio::test]
     async fn balance_requirement_check() {
