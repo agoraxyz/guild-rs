9f7d36d2b2304d31820c3b2b1e79d2764d5b4fca
richardanyalai <gtaricsi@gmail.com>
2023-02-24 20:51:12 +0100

    Implemented Role access checking logic

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -3,20 +3,127 @@
 #![deny(clippy::cargo)]
 #![deny(unused_crate_dependencies)]
 
-use guild_common as _;
-use guild_requirements as _;
+use async_trait::async_trait;
+use futures::future::join_all;
+use guild_common::{Identity, Requirement, Retrievable, Role, User};
+use guild_requirements::{AllowList, Balance, Free};
+use primitive_types::{H160 as Address, U256};
 use requiem as _;
+use std::{collections::HashMap, str::FromStr};
+
+#[async_trait]
+trait Checkable {
+    async fn check(&self, user: &User) -> Result<bool, ()>;
+    async fn check_batch(&self, users: &[User]) -> Result<Vec<bool>, ()>;
+}
+
+#[async_trait]
+impl Checkable for Role {
+    async fn check(&self, user: &User) -> Result<bool, ()> {
+        self.check_batch(&[user.clone()])
+            .await
+            .map(|accesses| accesses[0])
+    }
+
+    async fn check_batch(&self, users: &[User]) -> Result<Vec<bool>, ()> {
+        let users_count = users.len();
+        let ids: Vec<u64> = users.iter().map(|user| user.id).collect();
+        let id_addresses: Vec<(u64, Address)> = users
+            .iter()
+            .flat_map(|user| {
+                user.identities
+                    .iter()
+                    .filter_map(|identity| match identity {
+                        Identity::EvmAddress(address) => Some((user.id, *address)),
+                        _ => None,
+                    })
+            })
+            .collect();
+        let addresses: Vec<Address> = id_addresses.iter().map(|(_, address)| *address).collect();
+
+        let reduce_accesses = |accesses: &[bool]| -> Vec<bool> {
+            let id_accesses = id_addresses
+                .iter()
+                .zip(accesses.iter())
+                .map(|((user_id, _), access)| (*user_id, *access))
+                .collect::<Vec<(u64, bool)>>();
+
+            ids.iter()
+                .map(|id| {
+                    id_accesses
+                        .iter()
+                        .filter_map(|(i, access)| if id == i { Some(access) } else { None })
+                        .cloned()
+                        .reduce(|a, b| a || b)
+                        .unwrap_or_default()
+                })
+                .collect()
+        };
+
+        let accesses_per_req = join_all(self.requirements.iter().map(|req| async {
+            if let Some(free) = req.downcast_ref::<Free>() {
+                free.verify_batch(&vec![(); users_count])
+            } else if let Some(allowlist) = req.downcast_ref::<AllowList<Address>>() {
+                reduce_accesses(&allowlist.verify_batch(&addresses))
+            } else if let Some(balance_check) = req.downcast_ref::<Balance<Address, U256, U256>>() {
+                let balances = balance_check
+                    .retrieve_batch(&reqwest::Client::new(), &addresses)
+                    .await
+                    .unwrap();
+
+                reduce_accesses(&balance_check.verify_batch(&balances))
+            } else {
+                vec![false; users_count]
+            }
+        }))
+        .await;
+
+        let mut rotated = Vec::new();
+
+        for col in 0..accesses_per_req[0].len() {
+            let mut row = Vec::new();
+
+            for _row in &accesses_per_req {
+                if let Some(&c) = _row.get(col) {
+                    row.push(c);
+                }
+            }
+
+            rotated.push(row);
+        }
+
+        match requiem::LogicTree::from_str(&self.logic) {
+            Ok(tree) => {
+                let res = rotated
+                    .iter()
+                    .map(|accesses| {
+                        let mut terminals = HashMap::new();
+
+                        for (idx, access) in accesses.iter().enumerate() {
+                            terminals.insert(idx as u32, *access);
+                        }
+
+                        tree.evaluate(&terminals).unwrap_or(false)
+                    })
+                    .collect();
+                Ok(res)
+            }
+            Err(e) => Ok(vec![false; users_count]),
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
-    use guild_common::{address, Identity, Relation, Requirement, Role, TokenType, User};
+    use crate::Checkable;
+    use guild_common::{address, Identity, Relation, Role, TokenType, User};
     use guild_providers::evm::EvmChain;
     use guild_requirements::{AllowList, Balance, Free};
-    use primitive_types::{H160 as Address, U256};
-    use std::{any::Any, collections::HashMap, str::FromStr};
+    use primitive_types::U256;
+    use std::any::Any;
 
-    #[test]
-    fn role_check() {
+    #[tokio::test]
+    async fn role_check() {
         let allowlist = AllowList {
             deny_list: false,
             verification_data: vec![
@@ -42,7 +149,7 @@ mod test {
             relation: Relation::GreaterThan(U256::from(0)),
         };
 
-        let requirements: Vec<Box<dyn Any>> = vec![
+        let requirements: Vec<Box<dyn Send + Sync + Any>> = vec![
             Box::new(Free),
             Box::new(allowlist),
             Box::new(denylist),
@@ -55,58 +162,23 @@ mod test {
             requirements,
         };
 
-        let user = User {
+        let user1 = User {
+            id: 69,
             identities: vec![Identity::EvmAddress(address!(
                 "0xE43878Ce78934fe8007748FF481f03B8Ee3b97DE"
             ))],
         };
 
-        let addresses = user
-            .identities
-            .iter()
-            .filter_map(|identity| match identity {
-                Identity::EvmAddress(address) => Some(address),
-                _ => None,
-            })
-            .cloned()
-            .collect::<Vec<Address>>();
-
-        let accesses: Vec<bool> = role
-            .requirements
-            .iter()
-            .map(|req| {
-                if let Some(free) = req.downcast_ref::<Free>() {
-                    free.verify(&())
-                } else if let Some(allowlist) = req.downcast_ref::<AllowList<Address>>() {
-                    allowlist
-                        .verify_batch(&addresses)
-                        .iter()
-                        .cloned()
-                        .reduce(|a, b| a && b)
-                        .unwrap_or(false)
-                } else if let Some(balance_check) =
-                    req.downcast_ref::<Balance<Address, U256, U256>>()
-                {
-                    balance_check.verify(&U256::from(69))
-                } else {
-                    false
-                }
-            })
-            .collect();
-
-        let access = match requiem::LogicTree::from_str(&role.logic) {
-            Ok(tree) => {
-                let mut terminals = HashMap::new();
-
-                for (idx, access) in accesses.iter().enumerate() {
-                    terminals.insert(idx as u32, *access);
-                }
-
-                tree.evaluate(&terminals).unwrap_or(false)
-            }
-            Err(_) => false,
+        let user2 = User {
+            id: 420,
+            identities: vec![Identity::EvmAddress(address!(
+                "0x283d678711daa088640c86a1ad3f12c00ec1252e"
+            ))],
         };
 
-        assert!(access);
+        assert_eq!(
+            role.check_batch(&[user1, user2]).await.unwrap(),
+            &[true, false]
+        );
     }
 }
