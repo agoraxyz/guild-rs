d5983996f0985f7458b88ce2fd857fba5975e35f
richardanyalai <gtaricsi@gmail.com>
2023-03-16 15:47:12 +0100

    Improved TokenType conversion

--- requirements/src/balance.rs
+++ requirements/src/balance.rs
@@ -11,33 +11,38 @@ pub struct Balance {
 
 impl From<Balance> for Requirement {
     fn from(value: Balance) -> Self {
-        let (typ, address, id) = match value.token_type {
-            TokenType::Native => ("native".to_string(), "".to_string(), "".to_string()),
-            TokenType::Fungible { address } => ("fungible".to_string(), address, "".to_string()),
-            TokenType::NonFungible { address, id } => {
-                ("non_fungible".to_string(), address, id.unwrap_or_default())
-            }
-            TokenType::Special { address, id } => {
-                ("special".to_string(), address, id.unwrap_or_default())
-            }
+        let data = match value.token_type {
+            TokenType::Native => json!({
+                "type": "native"
+            }),
+            TokenType::Fungible { address } => json!({
+                "type": "fungible",
+                "address": address
+            }),
+            TokenType::NonFungible { address, id } => json!({
+                "type": "non_fungible",
+                "address": address,
+                "id": id
+            }),
+            TokenType::Special { address, id } => json!({
+                "type": "special",
+                "address": address,
+                "id": id
+            }),
         };
 
         let request = Request {
             base_url: value.chain.to_string(),
             method: Method::Get,
-            data: Data::JsonBody(json!({
-                "type": typ,
-                "address": address,
-                "id": id
-            })),
+            data: Data::JsonBody(data),
             auth: Auth::None,
             path: vec![],
         };
 
         Self {
-            type_id: format!("{:?}", RequirementType::EvmBalance).to_lowercase(),
+            type_id: RequirementType::EvmBalance.to_string(),
             request,
-            identity_id: "evmaddress".to_string(),
+            identity_id: "evm_address".to_string(),
             relation: value.relation,
         }
     }
