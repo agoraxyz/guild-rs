dd2584de256475ea5b1d940e88a07ef7b45fdef9
richardanyalai <gtaricsi@gmail.com>
2023-03-16 15:46:17 +0100

    Updated Chain to_string logic

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,13 +4,12 @@
 #![deny(unused_crate_dependencies)]
 
 pub use requirement::*;
-use std::fmt::Display;
+use std::fmt;
 pub use user::*;
 
 mod requirement;
 mod user;
 
-#[derive(Debug)]
 pub enum Chain {
     Ethereum,
     Polygon,
@@ -20,9 +19,23 @@ pub enum Chain {
     Arbitrum,
 }
 
-impl Display for Chain {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let lower = format!("{self:?}").to_lowercase();
-        write!(f, "{lower}")
+impl fmt::Debug for Chain {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let res = match self {
+            Self::Ethereum => "ethereum",
+            Self::Polygon => "polygon",
+            Self::Gnosis => "gnosis",
+            Self::Bsc => "bsc",
+            Self::Goerli => "goerli",
+            Self::Arbitrum => "arbitrum",
+        };
+
+        write!(f, "{res}")
+    }
+}
+
+impl fmt::Display for Chain {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <Self as fmt::Debug>::fmt(self, f)
     }
 }
