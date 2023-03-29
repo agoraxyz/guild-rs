ac02dfecc39284b40d50d222f0aa2536964c6d59
richardanyalai <gtaricsi@gmail.com>
2023-03-29 12:25:26 +0200

    Using &str instead of Value

--- requirements/evm_balance/src/lib.rs
+++ requirements/evm_balance/src/lib.rs
@@ -8,19 +8,18 @@ mod providers;
 use guild_common::{Relation, RequirementResult, TokenType, User};
 use providers::EvmProvider;
 use reqwest::Client;
-use serde_json::Value;
 use tokio::runtime;
 
 #[no_mangle]
 pub fn check(
     client: &'static Client,
     users: &[User],
-    metadata: Value,
-    secrets: Value,
+    metadata: &str,
+    secrets: &str,
 ) -> RequirementResult {
-    let provider: EvmProvider = serde_json::from_value(secrets).unwrap();
+    let provider: EvmProvider = serde_json::from_str(secrets).unwrap();
     let (token_type, relation): (TokenType, Relation<f64>) =
-        serde_json::from_value(metadata).unwrap();
+        serde_json::from_str(metadata).unwrap();
 
     let addresses: Vec<String> = users
         .iter()
