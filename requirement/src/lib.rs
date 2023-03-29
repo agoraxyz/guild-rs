7b267b871d5cbf8d6e907ac576f6eb8b2df879cf
richardanyalai <gtaricsi@gmail.com>
2023-03-29 11:10:40 +0200

    Removed redundant crates

--- requirement/src/lib.rs
+++ requirement/src/lib.rs
@@ -2,7 +2,3 @@
 #![deny(clippy::dbg_macro)]
 #![deny(clippy::cargo)]
 #![deny(unused_crate_dependencies)]
-
-use guild_common::{Requirement, RequirementError};
-
-mod variants;
--- requirements/src/variants/mod.rs
+++
@@ -1,3 +0,0 @@
-mod core;
-
-pub use self::core::{allowlist::AllowList, balance::Balance, free::Free};
