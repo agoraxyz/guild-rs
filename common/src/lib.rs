290104078568ecd2d4966a06e9263d87f3ec789a
PopcornPaws <lipschitz77@gmail.com>
2023-05-05 11:56:50 +0200

    WIP

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(feature = "std"), no_std)]
 #![deny(clippy::all)]
 #![deny(clippy::dbg_macro)]
 #![deny(clippy::cargo)]
@@ -5,48 +6,7 @@
 
 pub type Scalar = f64;
 
-/*
-pub use requirement::*;
-use std::fmt;
-pub use user::*;
-
-mod requirement;
-mod user;
-
-#[derive(Clone, Copy)]
-pub enum Chain {
-    Ethereum,
-    Polygon,
-    Gnosis,
-    Bsc,
-    Goerli,
-    Arbitrum,
-    SolanaMain,
-    SolanaTest,
-    SolanaDev,
-}
-
-impl fmt::Debug for Chain {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let res = match self {
-            Self::Ethereum => "ethereum",
-            Self::Polygon => "polygon",
-            Self::Gnosis => "gnosis",
-            Self::Bsc => "bsc",
-            Self::Goerli => "goerli",
-            Self::Arbitrum => "arbitrum",
-            Self::SolanaMain => "solana_main",
-            Self::SolanaTest => "solana_test",
-            Self::SolanaDev => "solana_dev",
-        };
-
-        write!(f, "{res}")
-    }
-}
-
-impl fmt::Display for Chain {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        <Self as fmt::Debug>::fmt(self, f)
-    }
-}
-*/
+#[cfg(not(feature = "std"))]
+pub use parity_scale_codec::alloc::vec::Vec;
+#[cfg(not(feature = "std"))]
+pub use parity_scale_codec::alloc::String;
